65339be1588e9dd26263481e31b4887059234338
Robert Morris <robert.dale.morris@gmail.com>
2018-01-16 12:03:33 -0500

    Update firebase.credentials.ts

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,8 +1,3 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyB7_mWbMUwGv7bXUEDU32TjehSqftFkLfI",
-    authDomain: "ionic-shopping-list-713ac.firebaseapp.com",
-    databaseURL: "https://ionic-shopping-list-713ac.firebaseio.com",
-    projectId: "ionic-shopping-list-713ac",
-    storageBucket: "",
-    messagingSenderId: "866411436299"
+//     Place your firbase config here to get your app up and running!!!!
   };
